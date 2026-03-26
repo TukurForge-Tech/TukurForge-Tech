8783327c77b62de0dca2d5d97764a2121f9b447a
M0rf30H <homactuario@gmail.com>
2026-03-26 17:55:14 -0600

    Update supabase-client.js

--- SimuTukur/supabase-client.js
+++ SimuTukur/supabase-client.js
@@ -2,8 +2,8 @@
 // Configuración centralizada de la base de datos de SimuTukur
 
 const supabaseUrl = 'https://pcuopqvmucmhtcdeswxh.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjdW9wcXZtdWNtaHRjZGVzd3hoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3Mzg1OTQ1OCwiZXhwIjoyMDg5NDM1NDU4fQ.K5p2k2z8i2mLQhlxsP2voILlos2hELhKbAVPNH2_p30';
-
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjdW9wcXZtdWNtaHRjZGVzd3hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NTk0NTgsImV4cCI6MjA4OTQzNTQ1OH0.g1xfdTtbz4aNW_QF_JcwoM7DCmgRLDxF2Ik48I7fq2M';
+                    
 // Exportamos el cliente para que cualquier HTML lo pueda usar
 const _supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
