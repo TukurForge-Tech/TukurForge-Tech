8ff2f7cb8aea40aa378a4abd69dcf571b1aa66b5
M0rf30H <homactuario@gmail.com>
2026-04-09 21:53:41 -0600

    Update simulacro_dash.js

--- SimuTukur/simulacro_dash.js
+++ SimuTukur/simulacro_dash.js
@@ -27,7 +27,9 @@ async function validarAccesoPilotoDash() {
 
         // Adecuación 2: Ponemos el nombre del papa en el saludo
         const saludoSpan = document.getElementById('username-display');
-        saludoSpan.innerText = `Saludo, ${data.nombre_tutor || email}`;
+        if (saludoSpan) {
+            saludoSpan.innerText = data.nombre_tutor || email;
+        }
 
     } catch (err) {
         console.error("Error en validación dash:", err);
